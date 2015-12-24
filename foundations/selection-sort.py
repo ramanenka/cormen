bb5a30e35541fa8b9a1aec7882a2242a7a2571c6
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2015-12-25 00:55:50 +0100

    Fix indentations

--- foundations/selection-sort.py
+++ foundations/selection-sort.py
@@ -2,13 +2,13 @@ a = [1, 4, 3, 2, 5]
 
 
 def selection_sort(a): 
-    for i in range(0, len(a) - 1):				# c1(n-1)
-        minIndex = i 							# c2(n-1)
-        for j in range(i + 1, len(a)): 			# c3((n-1) + (n-2) + ... + (n-(n-1)) = n^2/2 - n/2)
-            if a[j] < a[minIndex]:				# c4((n-1) + (n-2) + ... + (n-(n-1)) = n^2/2 - n/2)
-                minIndex = j					# c5*ti (best: ti = 0, worst ti = n^2/2 - n/2)
+    for i in range(0, len(a) - 1):              # c1(n-1)
+        minIndex = i                            # c2(n-1)
+        for j in range(i + 1, len(a)):          # c3((n-1) + (n-2) + ... + (n-(n-1)) = n^2/2 - n/2)
+            if a[j] < a[minIndex]:              # c4((n-1) + (n-2) + ... + (n-(n-1)) = n^2/2 - n/2)
+                minIndex = j                    # c5*ti (best: ti = 0, worst ti = n^2/2 - n/2)
 
-        a[i], a[minIndex] = a[minIndex], a[i]	# c6(n-1)
+        a[i], a[minIndex] = a[minIndex], a[i]   # c6(n-1)
 
 selection_sort(a)
 print(a)
